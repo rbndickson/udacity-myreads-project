3681e7e1fd255d402f06e4409016121edf39067d
Robin Dickson <robindickson@gmail.com>
2017-07-21 21:42:39 +0200

    refactor: Move Book to it's own component

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Book from './Book'
 
 class Bookshelf extends Component {
   booksOnShelf = (shelf) => {
@@ -14,24 +15,7 @@ class Bookshelf extends Component {
         <div className="bookshelf-books">
           <ol className="books-grid">
             {this.booksOnShelf(this.props.value).map((book) => (
-               <li key={book.id}>
-                 <div className="book">
-                   <div className="book-top">
-                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: "url(" + book.imageLinks.smallThumbnail + ")" }}></div>
-                     <div className="book-shelf-changer">
-                       <select>
-                         <option value="none" disabled>Move to...</option>
-                         <option value="currentlyReading">Currently Reading</option>
-                         <option value="wantToRead">Want to Read</option>
-                         <option value="read">Read</option>
-                         <option value="none">None</option>
-                       </select>
-                     </div>
-                   </div>
-                   <div className="book-title">{ book.title }</div>
-                   <div className="book-authors">{ book.author }</div>
-                 </div>
-               </li>
+              <Book book={book}/>
              ))}
           </ol>
         </div>
