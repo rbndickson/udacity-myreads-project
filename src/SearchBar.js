5cceb50e8e6d2b8d053d99d8984c5199ef9fc1e6
Robin Dickson <robindickson@gmail.com>
2017-07-29 15:27:34 +0200

    refactor: Simpify SearchBar
    Change to a stateless functional component.

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -2,22 +2,18 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './App.css'
 
-class SearchBar extends React.Component {
-  render() {
-    return (
-      <div className="search-books-bar">
-        <Link className="close-search" to="/">Close</Link>
-        <div className="search-books-input-wrapper">
-          <input
-            type="text"
-            placeholder="Search by title or author"
-            value={this.props.query}
-            onChange={(e) => { this.props.onUpdateSearchPage(e.target.value) }}
-          />
-        </div>
-      </div>
-    )
-  }
-}
+const SearchBar = (props) => (
+  <div className="search-books-bar">
+    <Link className="close-search" to="/">Close</Link>
+    <div className="search-books-input-wrapper">
+      <input
+        type="text"
+        placeholder="Search by title or author"
+        value={props.query}
+        onChange={(e) => { props.onUpdateSearchPage(e.target.value) }}
+      />
+    </div>
+  </div>
+)
 
 export default SearchBar
