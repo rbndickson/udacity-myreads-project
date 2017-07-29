0ef2417b813c5acf637c24cead2b890d5666f04f
Robin Dickson <robindickson@gmail.com>
2017-07-29 15:22:58 +0200

    refactor: Simplify ListBooks
    Change ListBooks to a stateless functional component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import Bookshelf from './Bookshelf'
 
-class ListBooks extends Component {
-  render() {
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <Bookshelf books={this.props.books} onUpdateBook={this.props.onUpdateBook} title="Currently Reading" value="currentlyReading" />
-        <Bookshelf books={this.props.books} onUpdateBook={this.props.onUpdateBook} title="Want to Read" value="wantToRead" />
-        <Bookshelf books={this.props.books} onUpdateBook={this.props.onUpdateBook} title="Read" value="read" />
-        <div className="open-search">
-          <Link to="/search">
-            Add a book
-          </Link>
-        </div>
-      </div>
-    )
-  }
-}
+const ListBooks = (props) => (
+  <div className="list-books">
+    <div className="list-books-title">
+      <h1>MyReads</h1>
+    </div>
+    <Bookshelf books={props.books} onUpdateBook={props.onUpdateBook} title="Currently Reading" value="currentlyReading" />
+    <Bookshelf books={props.books} onUpdateBook={props.onUpdateBook} title="Want to Read" value="wantToRead" />
+    <Bookshelf books={props.books} onUpdateBook={props.onUpdateBook} title="Read" value="read" />
+    <div className="open-search">
+      <Link to="/search">
+        Add a book
+      </Link>
+    </div>
+  </div>
+)
 
 export default ListBooks
