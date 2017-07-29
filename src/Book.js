42567c76605edc99916a07b068847808156783f4
Robin Dickson <robindickson@gmail.com>
2017-07-29 16:17:59 +0200

    refactor: Use template literal for image URL
    Rather than concatnating.

--- src/Book.js
+++ src/Book.js
@@ -16,7 +16,7 @@ class Book extends Component {
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: "url(" + this.props.book.imageLinks.smallThumbnail + ")" }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.smallThumbnail})` }}></div>
           <div className="book-shelf-changer">
             <select
               defaultValue={this.props.book.shelf}
