ad55389428e367776bc5472b2e58bc9b1c85c5e7
Robin Dickson <robindickson@gmail.com>
2017-08-01 16:59:28 +0200

    refactor: Change SearchBar component to a class
    So that propTypes checks can be added.

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const SearchBar = (props) => (
-  <div className="search-books-bar">
-    <Link className="close-search" to="/">Close</Link>
-    <div className="search-books-input-wrapper">
-      <input
-        type="text"
-        placeholder="Search by title or author"
-        value={props.query}
-        onChange={(e) => { props.onUpdateSearchPage(e.target.value) }}
-      />
-    </div>
-  </div>
-)
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div className="search-books-bar">
+        <Link className="close-search" to="/">Close</Link>
+        <div className="search-books-input-wrapper">
+          <input
+            type="text"
+            placeholder="Search by title or author"
+            value={this.props.query}
+            onChange={(e) => { this.props.onUpdateSearchPage(e.target.value) }}
+          />
+        </div>
+      </div>
+    )
+  }
+}
 
 export default SearchBar
