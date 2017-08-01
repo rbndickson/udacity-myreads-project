c9ec505ee1f5c61ac81c959c1e5639e058105bc1
Robin Dickson <robindickson@gmail.com>
2017-08-01 16:52:42 +0200

    refactor: Change ListBooks to a Component class
    So that propTypes checks can be added.

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -2,20 +2,24 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Bookshelf from './Bookshelf'
 
-const ListBooks = (props) => (
-  <div className="list-books">
-    <div className="list-books-title">
-      <h1>MyReads</h1>
-    </div>
-    <Bookshelf books={props.books} onUpdateBook={props.onUpdateBook} title="Currently Reading" value="currentlyReading" />
-    <Bookshelf books={props.books} onUpdateBook={props.onUpdateBook} title="Want to Read" value="wantToRead" />
-    <Bookshelf books={props.books} onUpdateBook={props.onUpdateBook} title="Read" value="read" />
-    <div className="open-search">
-      <Link to="/search">
-        Add a book
-      </Link>
-    </div>
-  </div>
-)
+class ListBooks extends React.Component {
+  render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <Bookshelf books={this.props.books} onUpdateBook={this.props.onUpdateBook} title="Currently Reading" value="currentlyReading" />
+        <Bookshelf books={this.props.books} onUpdateBook={this.props.onUpdateBook} title="Want to Read" value="wantToRead" />
+        <Bookshelf books={this.props.books} onUpdateBook={this.props.onUpdateBook} title="Read" value="read" />
+        <div className="open-search">
+          <Link to="/search">
+            Add a book
+          </Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default ListBooks
